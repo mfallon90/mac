3fabf94e5b92b70fb8d3972c846809e17be2dadd
mfallon <mtfallon91@gmail.com>
2024-01-14 11:32:35 -0600

    initial commit

---
+++ sim/run.py
@@ -0,0 +1,36 @@
+import os
+from cocotb_test.simulator import run
+import sys
+
+
+##########################################
+##           TEST SETUP                 ##
+##########################################
+
+root = os.path.abspath('.')
+hdl_dir = os.path.join(root, 'hdl')
+sim_dir = os.path.join(root, 'sim')
+dep_dir = os.path.join(root, 'deps')
+sys.path.extend([sim_dir, dep_dir])
+
+top = "rgmii_rx"
+modules = ["test_rgmii_rx"]
+
+def test(top, test):
+    run(
+        verilog_sources=[
+            os.path.join(hdl_dir,"rgmii_rx.sv"),
+        ],
+        toplevel=top,
+        module=test
+    )
+
+
+##########################################
+##              RUN TEST                ##
+##########################################
+
+
+if __name__ == "__main__":
+    for module in modules:
+        test(top, module)
